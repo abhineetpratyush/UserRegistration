79fbed23bb6d417cf2daf2bc9a8bd3649c8de571
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-02 17:05:06 +0530

    [abhineet] refactor : refactor to add ability to enter and validate last name

--- UserRegistration.java
+++ UserRegistration.java
@@ -2,8 +2,10 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public static void main(String[] args) {
-		String firstName;
+	private String firstName;
+	private String lastName;
+
+	private void addFirstName() {
 		boolean validCounter = false;
 		Scanner sc = new Scanner(System.in);
 		do {
@@ -16,6 +18,26 @@ public class UserRegistration {
 			else
 				System.out.println("Invalid. Please enter again!");
 		} while(!validCounter);
+	}
+	
+	private void addLastName() {
+		boolean validCounter = false;
+		Scanner sc = new Scanner(System.in);
+		do {
+			System.out.println("Last Name: ");
+			lastName = sc.nextLine();
+			if(Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lastName)) {
+				validCounter = true;
+				System.out.println("Valid!");
+			}
+			else
+				System.out.println("Invalid. Please enter again!");
+		} while(!validCounter);
 		sc.close();
+	}
+	public static void main(String[] args) {
+		UserRegistration userOne = new UserRegistration();
+		userOne.addFirstName();
+		userOne.addLastName();
 	}
 }
